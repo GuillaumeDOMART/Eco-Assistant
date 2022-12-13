5dbaa6e4231b317e4b71f5925fda8d12f66ba839
@clemcir <clementcirou22@gmail.com>
2022-12-13 10:49:19 +0100

    graph example without modif on interactions

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,16 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import {Form} from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import Chart from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
 
+
+const labels = ["Planification", "Développement", "Test", "Déploiement", "Maintenance"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: labels[0],
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
 function App() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
+  const myChart = new Chart()
 
   console.log(watch("example")); // watch input value by passing the name of it
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -21,9 +37,14 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          suiiiiiiiiiiiiiiiiii
+          suiiiiiiiiiiii
         </a>
+        <div id="myChart">
+          <Bar data={data} />
+        </div>
+        <div id="myChartv2">
 
+        </div>
         {/*---------React-BootStrap---------*/}
         <>
           <Form.Label>Range</Form.Label>
@@ -49,5 +70,30 @@ function App() {
     </div>
   );
 }
-
+function generateChart() {
+  const context = document.getElementById("myChartv2").getContext('2d');
+  const myChart = new Chart(context, {
+    type: 'bar',
+    data: data,
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      },
+      onHover(e) {
+        const activePoints = myChart.getElementsAtEventForMode(e, 'nearest', {
+          intersect: true
+        }, false)
+        const [{
+          index
+        }] = activePoints;
+        console.log(data.datasets[0].data[index]);
+      }
+    }
+  });
+  return myChart;
+}
 export default App;
