f91be9c0fc373031c12a7db40001a92abb837a33
guillaume LAGINHA <guillaume.laginha.dev@gmail.com>
2022-12-13 17:20:17 +0100

    ADD: connection to BDD

--- back_end/src/main/java/fr/eco_assistant/BackEndApplication.java
+++ back_end/src/main/java/fr/eco_assistant/BackEndApplication.java
@@ -5,18 +5,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class BackEndApplication {
     @Autowired
     private EmailSenderService senderService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     public static void main(String[] args) {
         SpringApplication.run(BackEndApplication.class, args);
     }
     @EventListener(ApplicationReadyEvent.class)
     public void sendMail() {
-        senderService.sendEmail("guillaumedomart@gmail.com",
-                "Test",
-                "This is theBody Test");
+        //senderService.sendEmail("guillaumedomart@gmail.com",
+        //        "Test",
+        //        "This is theBody Test");
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void connectBDD() {
+        var sql = "CREATE TABLE utilisateur(id INT PRIMARY KEY NOT NULL, nom VARCHAR(100))";
+        jdbcTemplate.execute(sql);
+    }
+
 }
