e3e55df8e3ea3bb93bdc44f683429282b7bbed0d
guillaume LAGINHA <guillaume.laginha.dev@gmail.com>
2022-12-14 09:33:22 +0100

    FIX: Test controller

--- back_end/src/main/java/fr/eco_assistant/TestController.java
+++ back_end/src/main/java/fr/eco_assistant/TestController.java
@@ -1,4 +1,4 @@
-package fr.eco_assistant.frontend;
+package fr.eco_assistant;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
