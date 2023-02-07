01862963b64596618427ff75d18b9502e3efab7f
cynthia-DL <cynthia.d.l@outlook.fr>
2023-02-07 11:28:38 +0100

    FIX : change questionSuiv to nullable integer

--- back/src/main/java/com/ecoassitant/back/DataBase/init.sql
+++ back/src/main/java/com/ecoassitant/back/DataBase/init.sql
@@ -2,25 +2,25 @@
     Initialisation DataBase
  */
 CREATE TABLE IF NOT EXISTS Profil (
-                                      idProfil serial PRIMARY KEY,
-                                      mail VARCHAR (140) NOT NULL,
+    idProfil serial PRIMARY KEY,
+    mail VARCHAR (140) NOT NULL,
     mdp VARCHAR (140) NOT NULL,
     nom VARCHAR (50) NOT NULL,
     prenom VARCHAR (50) NOT NULL,
     isAdmin INT NOT NULL);
 
 CREATE TABLE IF NOT EXISTS Projet (
-                                      idProjet serial PRIMARY KEY,
-                                      profilId INT NOT NULL,
-                                      nomProjet VARCHAR (50) NOT NULL,
+    idProjet serial PRIMARY KEY,
+    profilId INT NOT NULL,
+    nomProjet VARCHAR (50) NOT NULL,
     etat VARCHAR (50) NOT NULL,
     CONSTRAINT profilId
     FOREIGN KEY (profilId)
     REFERENCES Profil (idProfil));
 
 CREATE TABLE IF NOT EXISTS Question (
-                                        idQuestion serial PRIMARY KEY,
-                                        intitule VARCHAR (255) NOT NULL,
+    idQuestion serial PRIMARY KEY,
+    intitule VARCHAR (255) NOT NULL,
     questionPre INT,
     typeQ VARCHAR (140) NOT NULL,
     phase VARCHAR (50) NOT NULL,
@@ -31,16 +31,16 @@ CREATE TABLE IF NOT EXISTS Question (
     REFERENCES Question (idQuestion));
 
 CREATE TABLE IF NOT EXISTS Constante (
-                                         idConstante serial PRIMARY KEY,
-                                         constante INT,
-                                         tracabilite VARCHAR (255)
+    idConstante serial PRIMARY KEY,
+    constante INT,
+    tracabilite VARCHAR (255)
     );
 
 CREATE TABLE IF NOT EXISTS ReponsePossible (
-                                               idReponsePos serial PRIMARY KEY,
-                                               questionAsso INT NOT NULL,
-                                               questionSuiv INT NOT NULL,
-                                               intitule VARCHAR (140) NOT NULL,
+    idReponsePos serial PRIMARY KEY,
+    questionAsso INT NOT NULL,
+    questionSuiv INT,
+    intitule VARCHAR (140) NOT NULL,
     constanteId INT NOT NULL,
     CONSTRAINT questionAsso
     FOREIGN KEY (questionAsso)
@@ -50,11 +50,11 @@ CREATE TABLE IF NOT EXISTS ReponsePossible (
     REFERENCES Question (idQuestion));
 
 CREATE TABLE IF NOT EXISTS ReponseDonnee (
-                                             projetId INT NOT NULL,
-                                             reponsePosId INT NOT NULL,
-                                             entry INT NOT NULL,
-                                             CONSTRAINT projetId
-                                             PRIMARY KEY (projetId, reponsePosId),
+    projetId INT NOT NULL,
+    reponsePosId INT NOT NULL,
+    entry INT NOT NULL,
+    CONSTRAINT projetId
+    PRIMARY KEY (projetId, reponsePosId),
     FOREIGN KEY (projetId)
     REFERENCES Projet (idProjet),
     CONSTRAINT reponsePosId
@@ -62,15 +62,15 @@ CREATE TABLE IF NOT EXISTS ReponseDonnee (
     REFERENCES ReponsePossible (idReponsePos));
 
 CREATE TABLE IF NOT EXISTS CalculOperateur(
-                                              idCalculOp serial PRIMARY KEY,
-                                              operateur VARCHAR(5));
+      idCalculOp serial PRIMARY KEY,
+      operateur VARCHAR(5));
 
 CREATE TABLE IF NOT EXISTS Calcul (
-                                      calculOpId INT NOT NULL,
-                                      reponsePossibleId INT NOT NULL,
-                                      nbCalcul INT NOT NULL,
-                                      CONSTRAINT calculOpId
-                                      PRIMARY KEY (calculOpId, reponsePossibleId),
+    calculOpId INT NOT NULL,
+    reponsePossibleId INT NOT NULL,
+    nbCalcul INT NOT NULL,
+    CONSTRAINT calculOpId
+    PRIMARY KEY (calculOpId, reponsePossibleId),
     CONSTRAINT calculOpId2
     FOREIGN KEY (calculOpId)
     REFERENCES CalculOperateur (idCalculOp),
