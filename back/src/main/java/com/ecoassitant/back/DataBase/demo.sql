ebd88d021e5c372bdc92205ddb24a4ef20200077
cynthia-DL <cynthia.d.l@outlook.fr>
2023-02-07 11:28:02 +0100

    FIX : Correct SQL issues with demo.sql

--- back/src/main/java/com/ecoassitant/back/DataBase/demo.sql
+++ back/src/main/java/com/ecoassitant/back/DataBase/demo.sql
@@ -8,23 +8,24 @@ INSERT INTO Profil VALUES
 -- CREATION DES PROJETS DEVS ET SUPPORT
 INSERT INTO Projet VALUES
     (1, 2, 'QUESTIONAIRE POUR DEVELOPPEURS', 'INPROGRESS'),
-    (2, 3, "QUESTIONAIRE POUR L'EQUIPE SUPPORT", 'INPROGRESS')
+    (2, 3, 'QUESTIONAIRE POUR L EQUIPE SUPPORT', 'INPROGRESS');
 
 -- CREATION DES QUESTIONS
 INSERT INTO Question VALUES
-    (1, "Combien d'heures codez-vous par semaine ?", null, 'NUMERIC', 'DEVELOPPEMENT', 'FIRST', 1),
-    (2, "Utilisez-vous les méthodes agiles au sein de votre equipe ?", 1, 'QCM', 'PLANIFICATION', 'FIRST', 1),
-    (3, "Combiens de jours durre un sprint dans votre équipe", 2, 'NUMERIC', 'PLANIFICATION', 'FIRST', 0);
+    (1, 'Combien d heures codez-vous par semaine ?', null, 'NUMERIC', 'DEVELOPPEMENT', 'FIRST', 1),
+    (2, 'Utilisez-vous les méthodes agiles au sein de votre equipe ?', 1, 'QCM', 'PLANIFICATION', 'FIRST', 1),
+    (3, 'Combiens de jours durre un sprint dans votre équipe', 2, 'NUMERIC', 'PLANIFICATION', 'FIRST', 0);
 
 --- CREATION DES CONSTANTES
 INSERT INTO Constante VALUES
     (1, 1, 'source1.com'),
     (2, 1, 'source2.com'),
     (3, 1, 'source3.com'),
-    (4, 20, 'source4.com'),
+    (4, 20, 'source4.com');
+
 --- CREATION DES REPONSES POSSIBLE
 INSERT INTO ReponsePossible VALUES
-    (1, 1, 2, "Veuillez entrer un entier", 1),
+    (1, 1, 2, 'Veuillez entrer un entier', 1),
     (2, 2, 3, 'Oui',2),
     (3, 2, null, 'NON',3),
-    (4, 3, null, '"Veuillez entrer un entier"',4),
+    (4, 3, null, 'Veuillez entrer un entier',4);
