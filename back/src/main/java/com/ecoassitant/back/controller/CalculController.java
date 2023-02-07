d812a67cc72da35ea9e6e31730f2be91e0ec99bb
guillaume LAGINHA <guillaume.laginha.dev@gmail.com>
2023-02-07 17:01:50 +0100

    feat: update pom

---
+++ back/src/main/java/com/ecoassitant/back/controller/CalculController.java
@@ -0,0 +1,29 @@
+package com.ecoassitant.back.controller;
+
+import com.ecoassitant.back.entity.ReponseDonneeEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+
+@RequestMapping("api/calcul")
+@RestController
+public class CalculController {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public CalculController(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @GetMapping("run")
+    public String test(){
+        var list = entityManager.createQuery("SELECT rd FROM ReponseDonneeEntity rd JOIN ReponsePossibleEntity rp ON rd.reponseDonneeKey.reponsePos = rp JOIN ConstanteEntity c ON rp.constante = c JOIN CalculOperateurEntity co ON rp.WHERE rd.reponseDonneeKey.projet = :id", ReponseDonneeEntity.class)
+                .getResultList();
+        return "";
+    }
+
+}
