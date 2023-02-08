dcda3f7a4d9039a185ec8c10ca4d80cec5caedb0
@clemcir <clementcirou22@gmail.com>
2023-02-08 15:20:02 +0100

    created controller without methods retry for Profil because of IntellIJ

---
+++ back/src/main/java/com/ecoassitant/back/controller/ProfilController.java
@@ -0,0 +1,12 @@
+package com.ecoassitant.back.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/profil")
+@RestController
+public class ProfilController {
+    //TODO add methods
+    //fbhezb
+}
