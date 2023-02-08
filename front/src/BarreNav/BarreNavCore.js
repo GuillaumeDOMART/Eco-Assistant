149d195670b031d8b1790aecf9a7cb8d41d38042
cynthia-DL <cynthia.d.l@outlook.fr>
2023-02-08 11:54:52 +0100

    FIX: add comments

--- front/src/BarreNav/BarreNavCore.js
+++ front/src/BarreNav/BarreNavCore.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import logo from '../logo.svg';
-
+/**
+ * Composant qui représente la barre de navigation laérale.
+ * */
 function BarreNavCore() {
     return (
         <header className="col-2 order-12">
             <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-black">
-                <div className="position-sticky">
-                    <div className="list-group list-group-flush mx-3 mt-4">
+                <div className="position-sticky list-group list-group-flush mx-3 mt-4">
                         <img alt="logo react" src={logo} height="100px"></img>
                         {['Accueil', 'Profil', 'Remplir un questionnaire', 'Questions proposées', 'Moderation', 'Se déconnecter'].map(placement => (
                         <a href="./" className="list-group-item list-group-item-action py-2 ripple" aria-current="true">
-                            <i className="fas fa-tachometer-alt fa-fw me-3"></i><span>{placement}</span>
+                           <span key="placement">{placement}</span>
                         </a>
                         ))}
-                    </div>
                 </div>
             </nav>
         </header>
