2217695992d85ea70591b6b87365a94d755e1a88
cynthia-DL <cynthia.d.l@outlook.fr>
2023-02-08 12:18:46 +0100

    FIX: add react key (?)

--- front/src/BarreNav/BarreNavCore.js
+++ front/src/BarreNav/BarreNavCore.js
@@ -4,19 +4,19 @@ import logo from '../logo.svg';
  * Composant qui représente la barre de navigation laérale.
  * */
 function BarreNavCore() {
+    const listeOnglets = ['Accueil', 'Profil', 'Remplir un questionnaire', "Questions proposées", 'Moderation', 'Se déconnecter']
+    const listeLiens = listeOnglets.map((x, id) => (
+        <a href="./" className="list-group-item list-group-item-action py-2 ripple" key={id} aria-current="true">
+            <span key="key">{x}</span>
+        </a>
+    ))
     return (
-        <header className="col-2 order-12">
-            <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-black">
-                <div className="position-sticky list-group list-group-flush mx-3 mt-4">
-                        <img alt="logo react" src={logo} height="100px"></img>
-                        {['Accueil', 'Profil', 'Remplir un questionnaire', 'Questions proposées', 'Moderation', 'Se déconnecter'].map(placement => (
-                        <a href="./" className="list-group-item list-group-item-action py-2 ripple" aria-current="true">
-                           <span key="placement">{placement}</span>
-                        </a>
-                        ))}
-                </div>
-            </nav>
-        </header>
+        <nav id="sidebarMenu" className="col-2 order-12 collapse d-lg-block sidebar collapse bg-black">
+            <div className="position-sticky list-group list-group-flush mx-3 mt-4">
+                <img alt="logo react" src={logo} height="100px"></img>
+                {listeLiens}
+            </div>
+        </nav>
     );
 }
 
