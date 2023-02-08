007a6fd7bc90142cdcb8d0761357e487753581b0
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-08 14:41:27 +0100

    Fix: suppression des majuscule des tables

--- sql/init.sql
+++ sql/init.sql
@@ -1,7 +1,7 @@
 /**
     Initialisation DataBase
  */
-CREATE TABLE IF NOT EXISTS Profil (
+CREATE TABLE IF NOT EXISTS profil (
                 idProfil serial PRIMARY KEY,
                 mail VARCHAR (140) NOT NULL,
                 mdp VARCHAR (140) NOT NULL,
@@ -9,16 +9,16 @@ CREATE TABLE IF NOT EXISTS Profil (
                 prenom VARCHAR (50) NOT NULL,
                 isAdmin INT NOT NULL);
 
-CREATE TABLE IF NOT EXISTS Projet (
+CREATE TABLE IF NOT EXISTS projet (
                 idProjet serial PRIMARY KEY,
                 profilId INT NOT NULL,
                 nomProjet VARCHAR (50) NOT NULL,
                 etat VARCHAR (50) NOT NULL,
                 CONSTRAINT profilId
                 FOREIGN KEY (profilId)
-                    REFERENCES Profil (idProfil));
+                    REFERENCES profil (idProfil));
 
-CREATE TABLE IF NOT EXISTS Question (
+CREATE TABLE IF NOT EXISTS question (
                 idQuestion serial PRIMARY KEY,
                 intitule VARCHAR (255) NOT NULL,
                 questionPre INT,
@@ -28,15 +28,15 @@ CREATE TABLE IF NOT EXISTS Question (
                 visibilite INT,
                 CONSTRAINT questionPre
                 FOREIGN KEY (questionPre)
-                    REFERENCES Question (idQuestion));
+                    REFERENCES question (idQuestion));
 
-CREATE TABLE IF NOT EXISTS Constante (
+CREATE TABLE IF NOT EXISTS constante (
                 idConstante serial PRIMARY KEY,
                 constante INT,
                 tracabilite VARCHAR (255)
                 );
 
-CREATE TABLE IF NOT EXISTS ReponsePossible (
+CREATE TABLE IF NOT EXISTS reponsePossible (
                 idReponsePos serial PRIMARY KEY,
                 questionAsso INT NOT NULL,
                 questionSuiv INT,
@@ -44,28 +44,28 @@ CREATE TABLE IF NOT EXISTS ReponsePossible (
                 constanteId INT NOT NULL,
                 CONSTRAINT questionAsso
                 FOREIGN KEY (questionAsso)
-                    REFERENCES Question (idQuestion),
+                    REFERENCES question (idQuestion),
                 CONSTRAINT questionSuiv
                 FOREIGN KEY (questionSuiv)
-                    REFERENCES Question (idQuestion));
+                    REFERENCES question (idQuestion));
 
-CREATE TABLE IF NOT EXISTS ReponseDonnee (
+CREATE TABLE IF NOT EXISTS reponseDonnee (
                 projetId INT NOT NULL,
                 reponsePosId INT NOT NULL,
                 entry INT NOT NULL,
                 CONSTRAINT projetId
                 PRIMARY KEY (projetId, reponsePosId),
                 FOREIGN KEY (projetId)
-                    REFERENCES Projet (idProjet),
+                    REFERENCES projet (idProjet),
                 CONSTRAINT reponsePosId
                 FOREIGN KEY (reponsePosId)
-                    REFERENCES ReponsePossible (idReponsePos));
+                    REFERENCES reponsePossible (idReponsePos));
 
-CREATE TABLE IF NOT EXISTS CalculOperateur(
+CREATE TABLE IF NOT EXISTS calculOperateur(
                 idCalculOp serial PRIMARY KEY,
                 operateur VARCHAR(5));
 
-CREATE TABLE IF NOT EXISTS Calcul (
+CREATE TABLE IF NOT EXISTS calcul (
                 calculOpId INT NOT NULL,
                 reponsePossibleId INT NOT NULL,
                 nbCalcul INT NOT NULL,
@@ -73,8 +73,8 @@ CREATE TABLE IF NOT EXISTS Calcul (
                 PRIMARY KEY (calculOpId, reponsePossibleId),
                 CONSTRAINT calculOpId2
                 FOREIGN KEY (calculOpId)
-                    REFERENCES CalculOperateur (idCalculOp),
+                    REFERENCES calculOperateur (idCalculOp),
                 CONSTRAINT reponsePossibleId
                 FOREIGN KEY (reponsePossibleId)
-                    REFERENCES ReponsePossible (idReponsePos));
+                    REFERENCES reponsePossible (idReponsePos));
 
