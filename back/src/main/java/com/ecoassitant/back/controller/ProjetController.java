3cbf90e54fc493843fd2ff65e3af4ae7ecf793c5
cynthia-DL <cynthia.d.l@outlook.fr>
2023-02-09 14:44:18 +0100

    FIX documentation

--- back/src/main/java/com/ecoassitant/back/controller/ProjetController.java
+++ back/src/main/java/com/ecoassitant/back/controller/ProjetController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Class to manage endpoints regarding profiles
+ */
 @RequestMapping("api")
 @RestController
 public class ProjetController {
@@ -18,12 +21,18 @@ public class ProjetController {
         this.projetRepository = projetRepository;
     }
 
+    /**
+     * Endpoint to retrieve all projects
+     */
     @GetMapping("/projets")
     @ResponseBody
     public List<ProjetDto> listerLesProjets(){
         return projetRepository.findAll().stream().map(ProjetDto::new).toList();
     }
 
+    /**
+     * Endpoint to retrieve a projet by id
+     */
     @GetMapping("/projet/{id}")
     @ResponseBody
     public ProjetDto recupererProjetAvecId(@PathVariable("id") Long id){
@@ -31,6 +40,9 @@ public class ProjetController {
         return entity.map(ProjetDto::new).orElse(null);
     }
 
+    /**
+     * Endpoint to retrieve a projet by it's user's id
+     */
     @GetMapping("/projet/user/{id}")
     @ResponseBody
     public List<ProjetDto> recupererProjetAvecUserId(@PathVariable("id") Long id){
--- back/src/main/java/com/ecoassitant/back/service/ProfilService.java
+++ back/src/main/java/com/ecoassitant/back/service/ProfilService.java
@@ -2,7 +2,22 @@ package com.ecoassitant.back.service;
 
 import com.ecoassitant.back.dto.ProfilDto;
 
+/**
+ * Service for ProfilController
+ */
+
 public interface ProfilService {
-    public ProfilDto getProfilByID(Long id);
-    public ProfilDto getProfilByMail(String mail);
+    /**
+     * Retrieve a profil by it id
+     * @param id the profil id
+     * @return a ProfilDto with the submitted ID, or null if it doesn't exist
+     */
+    ProfilDto getProfilByID(Long id);
+
+    /**
+     * Retrieve a profil by it mail address
+     * @param mail the profil mail
+     * @return a ProfilDto with the submitted mail, or null if it doesn't exist
+     */
+    ProfilDto getProfilByMail(String mail);
 }
