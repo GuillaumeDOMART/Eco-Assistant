749ce38a97b9eb85691d07af45ea30b762b56b31
Guillaume Domart <guillaumedomart@gmail.com>
2023-02-09 13:52:02 +0100

    feat : premier affichage du questionnaire

--- front/src/Questionnaire.js
+++ front/src/Questionnaire.js
@@ -1,51 +1,115 @@
-import { useForm } from "react-hook-form";
-import React, {useEffect, useState} from "react";
+import React from "react";
 
-// The following component is an example of your existing Input Component
-const Input = ({ label, register, required }) => (
-    <>
-        <label>{label}</label>
-        <input {...register(label, { required })} />
-    </>
-);
+const test = {"intitule":"Combien d heures codez-vous par semaine ?",
+    "type":"NUMERIC",
+    "phase":"DEVELOPPEMENT",
+    "categorie":"FIRST",
+    "reponses":[
+        {
+            "questionSuiv":
+                {
+                    "intitule":"Utilisez-vous les méthodes agiles au sein de votre equipe ?",
+                    "type":"QCM",
+                    "phase":"PLANIFICATION",
+                    "categorie":"FIRST",
+                    "reponses":[
+                        {
+                            "questionSuiv":
+                                {
+                                    "intitule":"Combiens de jours durre un sprint dans votre équipe",
+                                    "type":"NUMERIC",
+                                    "phase":"PLANIFICATION",
+                                    "categorie":"FIRST",
+                                    "reponses":[
+                                        {
+                                            "questionSuiv":null,
+                                            "intitule":"Veuillez entrer un entier",
+                                            "constante":20
+                                        }]},
+                            "intitule":"Oui",
+                            "constante":1
+                        },
+                        {
+                            "questionSuiv":null,
+                            "intitule":"NON",
+                            "constante":1
+                        }]},
+            "intitule":"Veuillez entrer un entier","constante":1
+        }]
+}
 
-// you can use React.forwardRef to pass the ref too
-const Select = React.forwardRef(({ onChange, onBlur, name, label }, ref) => (
-    <>
-        <label>{label}</label>
-        <select name={name} ref={ref} onChange={onChange} onBlur={onBlur}>
-            <option value="20">20</option>
-            <option value="30">30</option>
-        </select>
-    </>
-));
+function buildQuestionnaire(test, arrayReturn = [], index = 0, reponseRequise = null) {
+    if (test === null)
+        return;
+    arrayReturn[index] = parseQuestion(test, reponseRequise)
+    test.reponses.map((value) => {
+        buildQuestionnaire(value.questionSuiv, arrayReturn, index + 1, value.intitule)
+    })
+    return arrayReturn;
+}
+
+function getResponses(question) {
+    const responsesArray = []
+    question.reponses.map((responses, index) => {
+        responsesArray[index] = responses.intitule
+
+    })
+    return responsesArray;
+}
+
+function parseQuestion(question, reponseRequise) {
+    return [reponseRequise, question.intitule, question.type, question.phase, question.categorie, getResponses(question)]
+}
+
+const QCM = (value) => {
+    return (
+        <>
+            <label>{value.question[1]}</label><br/>
+            <select>
+                {value.question[5].map((value) => {
+                    return <option value={value} key={value}> {value}</option>
+                })}
+            </select>
+            <input type="submit" style={{marginLeft: '10px'}}/>
+        </>
+    )
+}
+
+const NUMERIC = (value) => {
+    return (
+        <>
+            <label>{value.question[1]}</label><br/>
+            {value.question[5][0]} : <input/>
+            <input type="submit" style={{marginLeft: '10px'}}/>
+        </>
+    )
+}
 
 function Questionnaire() {
-    const { register, handleSubmit } = useForm();
-    const [questionnaire, onQuestionnaire] = useState([])
+    // const { register, handleSubmit } = useForm();
+    // const [questionnaire, onQuestionnaire] = useState([])
 
     const onSubmit = (data) => {
         alert(JSON.stringify(data));
     };
 
-    useEffect(() => {
-        var requestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-        };
-
-        fetch("/api/questions/all", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-    })
-
-
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <Input label="First Name" register={register} required />
-            <Select label="Age" {...register("Age")} />
-            <input type="submit" />
+            {buildQuestionnaire(test).map((value) => {
+                if (value[2] === 'QCM') {
+                    return (
+                        <div key={value}>
+                            <QCM question={value}/>
+                        </div>
+                    )}
+                {
+                    return (
+                        <div key={value}>
+                            <NUMERIC question={value}/>
+                        </div>
+                    )
+                }
+            })}
         </form>
     );
 }
