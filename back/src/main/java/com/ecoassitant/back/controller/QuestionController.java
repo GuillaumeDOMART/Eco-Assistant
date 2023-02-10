22ad131a95faba5b1ba0c4d504b6ccf31cabcfae
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-10 10:19:37 +0100

    Fix import useless

--- back/src/main/java/com/ecoassitant/back/controller/QuestionController.java
+++ back/src/main/java/com/ecoassitant/back/controller/QuestionController.java
@@ -1,11 +1,9 @@
 package com.ecoassitant.back.controller;
 
-import com.ecoassitant.back.dto.IdDto;
 import com.ecoassitant.back.dto.QuestionDto;
 import com.ecoassitant.back.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
