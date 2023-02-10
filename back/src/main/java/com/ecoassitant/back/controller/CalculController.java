0e077fb944b19b5a57a5b5afbcae7a4439ecf0e0
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-10 10:51:41 +0100

    Fix : doc

--- back/src/main/java/com/ecoassitant/back/controller/CalculController.java
+++ back/src/main/java/com/ecoassitant/back/controller/CalculController.java
@@ -6,7 +6,6 @@ import com.ecoassitant.back.service.CalculService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /**
  * Controller for request about calcul
