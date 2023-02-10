9f58ad55ffa037a2cc19f15f0cb88c9ae8a4c402
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-10 14:43:50 +0100

    Fix : correct init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -136,7 +136,8 @@ INSERT INTO reponsedonnee VALUES
 
 --CREATION CALCUL TEST
 INSERT INTO calcul VALUES
-(1, 3, 6, 1),
-(2, 3, 5, 1),
-(3, 3, 6, 2),
-(4, 5, 7, 2);
+(1, 1, 1, 1),
+(2, 3, 2, 1),
+(3, 5, 4, 1),
+(4, 3, 6, 2),
+(5, 5, 7, 2);
