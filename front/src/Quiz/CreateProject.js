65680960f1c8491bed1bae328445ae7360f9de4a
Guillaume Domart <guillaumedomart@gmail.com>
2023-02-16 09:58:25 +0100

    fix: amélioration code

--- front/src/Quiz/CreateProject.js
+++ front/src/Quiz/CreateProject.js
@@ -3,16 +3,28 @@ import {useForm} from "react-hook-form";
 import {Button, TextField} from "@mui/material";
 import {Col, Container, Row} from "react-bootstrap";
 
+
+function Project(onSubmit, register) {
+    return (
+        <Col className="">
+            <h1>Nouveau Projet</h1>
+            <form onSubmit={onSubmit} className="">
+                <TextField label="Nom du projet" type="text" variant="standard" className="textfield" {...register("projectName")} required/><br/>
+                <Button type="submit">Créer le projet</Button><br/>
+            </form>
+        </Col>
+    )
+}
 function CreateProject() {
     const navigate = useNavigate();
     const {register, handleSubmit} = useForm();
 
     const onSubmit = (datas) => {
-        var myHeaders = new Headers();
+        const myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${sessionStorage.getItem("token")}`);
         myHeaders.append("Content-Type", "application/json");
 
-        var requestOptions = {
+        const requestOptions = {
             method: 'POST',
             headers: myHeaders,
             body: JSON.stringify(datas),
@@ -20,22 +32,14 @@ function CreateProject() {
         };
 
         fetch("{{base_url}}/projet/create", requestOptions)
-            .then(response => console.log(response.text()))
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
+            .then(response => response.text())
         navigate("/questionnaire")
     }
 
     return (
         <Container fluid>
             <Row className="border border-5 vh-100">
-                <Col className="">
-                    <h1>Nouveau Projet</h1>
-                    <form onSubmit={handleSubmit(onSubmit)} className="">
-                        <TextField label="Nom du projet" type="text" variant="standard" className="textfield" {...register("projectName")} required/><br/>
-                        <Button type="submit">Créer le projet</Button><br/>
-                    </form>
-                </Col>
+                <Project onSubmit={handleSubmit(onSubmit)} register={register}/>
             </Row>
         </Container>
     );
