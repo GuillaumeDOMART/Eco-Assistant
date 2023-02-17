b6a560c915d5fc42bbe44255ab4e83d684439aff
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-17 11:50:59 +0100

    Feat : maj template

--- sql/init.sql
+++ sql/init.sql
@@ -111,16 +111,54 @@ INSERT INTO projet(profilid, nomprojet, etat) VALUES
 
 -- CREATION DES QUESTIONS
 INSERT INTO question (intitule, questionpre, typeq, phase, categorie, visibilite) VALUES
-                                                                                      ('Sur la duree totale du projet, combiens de jours avez-vous passer sur place (au bureau) ?', 6, 'NUMERIC', 'HORS_PHASE', 'FIRST', 1),
-                                                                                      ('Est-ce que vous vous déplacer à pied pour vous rendre sur place ?', 1, 'QCM', 'HORS_PHASE', 'FIRST', 1),
-                                                                                      ('Combiens de KM est-ce que vous parcourrez en voiture pour vous rendre sur place ?', 2, 'NUMERIC', 'HORS_PHASE', 'FIRST', 0),
-                                                                                      ('Combiens de salariées ont été mobilisé pour la phase de développements ?', 7, 'NUMERIC', 'DEVELOPPEMENT', 'FIRST', 1),
-                                                                                      ('Combiens de jours la phase de développements a-t-elle durée?', 4, 'NUMERIC', 'DEVELOPPEMENT', 'FIRST', 1),
-                                                                                      ('Combiens de jours au total le projet a-t-il / vas-t-il duré ?', null, 'NUMERIC', 'HORS_PHASE', 'FIRST', 1),
-                                                                                      ('Cest une question de planification?',3, 'QCM','PLANIFICATION', 'FIRST', 1),
-                                                                                      ('combien de test avez vous fait?', 5, 'NUMERIC', 'TEST', 'FIRST', 1),
-                                                                                      ('Maxime a mis en place le deploiement?', 8,'QCM', 'DEPLOIEMENT',  'FIRST', 1),
-                                                                                      ('ça maintient?', 9, 'QCM', 'MAINTENANCE',  'FIRST', 1);
+                                                                                      /*HORS_PHASE*/
+                                                                                      ('Votre projet, nécessite-t-il des trajets en avions ?', null, 'QCM', 'HORS_PHASE', 'FIRST', 1),
+                                                                                      ('Combien ?', 1, 'NUMERIC', 'HORS_PHASE', 'FIRST', 0),
+                                                                                      ('Produisez-vous de l’énergie verte ?', 2, 'QCM', 'HORS_PHASE', 'FIRST', 1),
+                                                                                      ('A qu’elle pourcentage couvre t’elle vos dépenses énergétique', 3, 'NUMERIC', 'HORS_PHASE', 'FIRST', 0),
+                                                                                      ('Combien de jours de télétravail par semaine ?', 4, 'NUMERIC', 'HORS_PHASE',  'FIRST', 1),
+                                                                                      /*PLANIFICATION*/
+                                                                                      ('Combien de collaborateur travaille sur cette phase ?', 5, 'NUMERIC', 'PLANIFICATION', 'FIRST', 1),
+                                                                                      ('Connaissez-vous à peu près la distance de trajet de vos collaborateurs ?', 6, 'QCM', 'PLANIFICATION',  'FIRST', 1),
+                                                                                      ('Quelle est la distance moyenne de trajet par personne pour aller au travail ?', 7, 'NUMERIC', 'PLANIFICATION',  'FIRST', 0),
+                                                                                      ('Combien vienne en voiture ?', 8, 'NUMERIC', 'PLANIFICATION',  'FIRST', 1),
+                                                                                      ('Combien de page de document avez-vous utilisé', 9, 'NUMERIC', 'PLANIFICATION',  'FIRST', 1),
+                                                                                      ('Dématérialisez-vous vos documents ?', 10, 'QCM', 'PLANIFICATION',  'FIRST', 1),
+                                                                                      ('Donner un pourcentage (de 0 à 100) de la dématérialisation de vos documents', 11, 'NUMERIC', 'PLANIFICATION',  'FIRST', 0),
+                                                                                      /*DEVELOPPEMENT*/
+                                                                                      ('Combien de collaborateur travaille sur cette phase ?', 12, 'NUMERIC', 'DEVELOPPEMENT', 'FIRST', 1),
+                                                                                      ('Connaissez-vous à peu près la distance de trajet de vos collaborateurs ?', 13, 'QCM', 'DEVELOPPEMENT',  'FIRST', 1),
+                                                                                      ('Quelle est la distance moyenne de trajet par personne pour aller au travail ?', 14, 'NUMERIC', 'DEVELOPPEMENT',  'FIRST', 0),
+                                                                                      ('Combien vienne en voiture ?', 15, 'NUMERIC', 'DEVELOPPEMENT',  'FIRST', 1),
+                                                                                      ('Combien de pc utilisez-vous pour cette phase ?', 16, 'NUMERIC', 'DEVELOPPEMENT',  'FIRST', 1),
+                                                                                      ('Utilisez-vous des machines virtuelles ?', 17, 'QCM', 'DEVELOPPEMENT',  'FIRST', 1),
+                                                                                      ('Combien ?', 18, 'NUMERIC', 'DEVELOPPEMENT', 'FIRST', 0),
+                                                                                      ('Quelle langage utilisez-vous pour développer en back ?', 19, 'QCM', 'DEVELOPPEMENT',  'FIRST', 1),
+                                                                                      ('Quelle langage utilisez-vous pour développer en front ?', 20, 'QCM', 'DEVELOPPEMENT',  'FIRST', 1),
+                                                                                      /*TEST*/
+                                                                                      ('Combien de collaborateur travaille sur cette phase ?', 21, 'NUMERIC', 'TEST', 'FIRST', 1),
+                                                                                      ('Connaissez-vous a peut prés la distance de trajet de vos collaborateurs ?', 22, 'QCM', 'TEST',  'FIRST', 1),
+                                                                                      ('Quelle est la distance moyenne de trajet par personne pour aller au travail ?', 23, 'NUMERIC', 'TEST',  'FIRST', 0),
+                                                                                      ('Combien vienne en voiture ?', 24, 'NUMERIC', 'TEST',  'FIRST', 1),
+                                                                                      ('Combien de pc utilisez-vous pour cette phase ?', 25, 'NUMERIC', 'TEST',  'FIRST', 1),
+                                                                                      /*DEPLOIEMENT*/
+                                                                                      ('Combien de collaborateur travaille sur cette phase ?', 26, 'NUMERIC', 'DEPLOIEMENT', 'FIRST', 1),
+                                                                                      ('Connaissez-vous à peu près la distance de trajet de vos collaborateurs ?', 27, 'QCM', 'DEPLOIEMENT',  'FIRST', 1),
+                                                                                      ('Quelle est la distance moyenne de trajet par personne pour aller au travail ?', 28, 'NUMERIC', 'DEPLOIEMENT',  'FIRST', 0),
+                                                                                      ('Combien vienne en voiture ?', 29, 'NUMERIC', 'DEPLOIEMENT',  'FIRST', 1),
+                                                                                      ('Combien de pc utilisez-vous pour cette phase ?', 30, 'NUMERIC', 'DEPLOIEMENT',  'FIRST', 1),
+                                                                                      ('Avez-vous une base de donnée ?', 31, 'QCM', 'DEPLOIEMENT',  'FIRST', 1),
+                                                                                      ('Quelle taille fait-elle ?', 32, 'NUMERIC', 'DEPLOIEMENT',  'FIRST', 0),
+                                                                                      ('Est-elle stockée dans le cloud ?', 33, 'QCM', 'DEPLOIEMENT',  'FIRST', 0),
+                                                                                      ('Savez-vous qu’elle énergie alimente votre cloud ?', 34, 'QCM', 'DEPLOIEMENT',  'FIRST', 0),
+                                                                                      ('Citez qu’elle énergie alimente votre Cloud ?', 35, 'QCM', 'DEPLOIEMENT',  'FIRST', 0),
+                                                                                      /*MAINTENANCE*/
+                                                                                      ('Faites-vous une phase de maintenance ?', 36, 'QCM', 'MAINTENANCE', 'FIRST', 1),
+                                                                                      ('Combien de collaborateur travaille sur cette phase ?', 37, 'NUMERIC', 'MAINTENANCE', 'FIRST', 1),
+                                                                                      ('Connaissez-vous à peu près la distance de trajet de vos collaborateurs ?', 38, 'QCM', 'MAINTENANCE',  'FIRST', 1),
+                                                                                      ('Quelle est la distance moyenne de trajet par personne pour aller au travail ?', 39, 'NUMERIC', 'MAINTENANCE',  'FIRST', 0),
+                                                                                      ('Combien vienne en voiture ?', 40, 'NUMERIC', 'MAINTENANCE',  'FIRST', 1);
+
 
 --- CREATION DES CONSTANTES
 INSERT INTO constante (constante, tracabilite) VALUES
@@ -132,21 +170,76 @@ INSERT INTO constante (constante, tracabilite) VALUES
 
 --- CREATION DES REPONSES POSSIBLE
 INSERT INTO reponsepossible (questionasso, questionsuiv, intitule, constanteid) VALUES
-                                                                                    (6, 1, 'Veuillez entrer un entier', 2),
-                                                                                    (1, 2, 'Veuillez entrer un entier', 2),
-                                                                                    (2, 3, 'OUI', 2),
-                                                                                    (2, 5, 'NON', 2),
-                                                                                    (3, 4, 'Veuillez entrer un entier', 4),
+                                                                                    /*HORS_PHASE*/
+                                                                                    (1, 2, 'OUI', 2),
+                                                                                    (1, 3, 'NON', 2),
+                                                                                    (2, 3, 'Veuillez entrer un entier', 2),
+                                                                                    (3, 4, 'OUI', 2),
+                                                                                    (3, 5, 'NON', 2),
                                                                                     (4, 5, 'Veuillez entrer un entier', 2),
-                                                                                    (5, 7, 'Veuillez entrer un entier', 3),
+                                                                                    (5, 6, 'Veuillez entrer un entier', 2),
+                                                                                    /*PLANIFICATION*/
+                                                                                    (6, 7, 'Veuillez entrer un entier', 2),
                                                                                     (7, 8, 'OUI', 2),
-                                                                                    (7, 8, 'NON', 2),
-                                                                                    (7, 8, 'REPONSE D', 2),
-                                                                                    (8, 9, 'Veuillez entrer un entier', 3),
-                                                                                    (9, 10, 'NON', 1),
-                                                                                    (9, 10, 'TOUJOURS NON', 1),
-                                                                                    (10, null, 'couci', 4),
-                                                                                    (10, null, 'couça', 4);
+                                                                                    (7, 9, 'NON', 2),
+                                                                                    (8, 9, 'Veuillez entrer un entier', 2),
+                                                                                    (9, 10, 'Veuillez entrer un entier', 2),
+                                                                                    (10, 11, 'Veuillez entrer un entier', 2),
+                                                                                    (11, 12, 'OUI', 2),
+                                                                                    (11, 13, 'NON', 2),
+                                                                                    (12, 13, 'Veuillez entrer un entier', 2),
+                                                                                    /*DEVELOPPEMENT*/
+                                                                                    (13, 14, 'Veuillez entrer un entier', 2),
+                                                                                    (14, 15, 'OUI', 2),
+                                                                                    (14, 16, 'NON', 2),
+                                                                                    (15, 16, 'Veuillez entrer un entier', 2),
+                                                                                    (16, 17, 'Veuillez entrer un entier', 2),
+                                                                                    (17, 18, 'Veuillez entrer un entier', 2),
+                                                                                    (18, 19, 'OUI', 2),
+                                                                                    (18, 19, 'NON', 2),
+                                                                                    (19, 20, 'C', 2),
+                                                                                    (19, 20, 'C++', 2),
+                                                                                    (19, 20, 'Java', 2),
+                                                                                    (19, 20, 'Python', 2),
+                                                                                    (19, 20, 'Autre', 2),
+                                                                                    (20, 21, 'PHP', 2),
+                                                                                    (20, 21, 'JavaScript', 2),
+                                                                                    (20, 21, 'TypeScript', 2),
+                                                                                    (20, 21, 'Autre', 2),
+                                                                                    /*TEST*/
+                                                                                    (21, 22, 'Veuillez entrer un entier', 2),
+                                                                                    (22, 23, 'OUI', 2),
+                                                                                    (22, 24, 'NON', 2),
+                                                                                    (23, 24, 'Veuillez entrer un entier', 2),
+                                                                                    (24, 25, 'Veuillez entrer un entier', 2),
+                                                                                    (25, 26, 'Veuillez entrer un entier', 2),
+
+                                                                                    /*DEPLOIEMENT*/
+                                                                                    (26, 27, 'Veuillez entrer un entier', 2),
+                                                                                    (27, 28, 'OUI', 2),
+                                                                                    (27, 29, 'NON', 2),
+                                                                                    (28, 29, 'Veuillez entrer un entier', 2),
+                                                                                    (29, 30, 'Veuillez entrer un entier', 2),
+                                                                                    (30, 31, 'OUI', 2),
+                                                                                    (30, 35, 'NON', 2),
+                                                                                    (31, 32, 'Veuillez entrer un entier', 2),
+                                                                                    (32, 33, 'OUI', 2),
+                                                                                    (32, 35, 'NON', 2),
+                                                                                    (33, 34, 'OUI', 2),
+                                                                                    (33, 35, 'NON', 2),
+                                                                                    (34, 35, 'NUCLEAIRE', 2),
+                                                                                    (34, 35, 'HYDRAULIQUE', 2),
+                                                                                    (34, 35, 'CHARBON', 2),
+                                                                                    (34, 35, 'GEOTHERMIQUE', 2),
+                                                                                    (34, 35, 'ENERGIE RENOUVLABLE', 2),
+                                                                                    /*MAINTENANCE*/
+                                                                                    (35, 36, 'OUI', 2),
+                                                                                    (35, null, 'NON', 2),
+                                                                                    (36, 37, 'Veuillez entrer un entier', 2),
+                                                                                    (38, 39, 'OUI', 2),
+                                                                                    (38, 40, 'NON', 2),
+                                                                                    (39, 40, 'Veuillez entrer un entier', 2),
+                                                                                    (40, null, 'Veuillez entrer un entier', 2);
 
 --CREATION REPONSEDONNEE
 INSERT INTO reponsedonnee VALUES
