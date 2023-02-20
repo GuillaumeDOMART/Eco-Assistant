6a6997ae93d1a836a54efebc4f886498eac5e48d
guillaume LAGINHA <guillaume.laginha.dev@gmail.com>
2023-02-20 14:11:36 +0100

    feat: add comment

--- back/src/main/java/com/ecoassitant/back/controller/AuthenticationController.java
+++ back/src/main/java/com/ecoassitant/back/controller/AuthenticationController.java
@@ -51,7 +51,7 @@ public class AuthenticationController {
 
     /**
      * Function to send email when the user forgot his password
-     * @param forgotMailInput
+     * @param forgotMailInput the mail of the user
      * @return
      */
     @PatchMapping("forgotMail")
