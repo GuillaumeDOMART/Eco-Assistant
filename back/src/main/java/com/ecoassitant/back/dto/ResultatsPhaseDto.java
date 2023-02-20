080244c6d530da13463651f0b8eadba5c543d4c7
guillaume LAGINHA <guillaume.laginha.dev@gmail.com>
2023-02-20 14:53:50 +0100

    feat: add doc

--- back/src/main/java/com/ecoassitant/back/dto/ResultatsPhaseDto.java
+++ back/src/main/java/com/ecoassitant/back/dto/ResultatsPhaseDto.java
@@ -6,9 +6,27 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Dto for result of a phase
+ */
 @Data
 public class ResultatsPhaseDto {
     private ResultatDto mine;
     private final List<ResultatDto> others = new ArrayList<>();
 
+    /**
+     * Builder of ResultatsPhaseDto
+     * @param mine the result Dto
+     */
+    public ResultatsPhaseDto(ResultatDto mine) {
+        this.mine = mine;
+    }
+
+    /**
+     * Function to add another Dto
+     * @param resultatDto the other Dto
+     */
+    public void addOther(ResultatDto resultatDto){
+        others.add(resultatDto);
+    }
 }
