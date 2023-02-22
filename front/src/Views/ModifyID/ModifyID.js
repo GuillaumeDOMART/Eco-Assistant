e5c7cca8da1f4bb248390313091c9614fdc0bb0f
Maxime Rodriguez <max.rodriguezidf@gmail.com>
2023-02-22 15:42:33 +0100

    front modifyID

--- front/src/Views/ModifyID/ModifyID.js
+++ front/src/Views/ModifyID/ModifyID.js
@@ -7,23 +7,54 @@ function ModifyID() {
     const {register, handleSubmit} = useForm()
     const navigate = useNavigate()
 
-    const onSubmit = () => {
-        //TODO modifyID
-        navigate("/profil")
+    const onSubmit = async (datas) => {
+        let token = sessionStorage.getItem("token");
+
+        // Token session expired
+        if (token === null) {
+            navigate("/");
+        }
+
+        let myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Bearer ${token}`)
+
+        const jsonBody = {newMail: datas.newMail}
+        const requestOptions = {
+            method: 'PATCH',
+            headers: myHeaders,
+            body: JSON.stringify(jsonBody),
+            redirect: 'follow'
+        };
+
+        let response = await fetch("api/auth/changeMail", requestOptions);
+        let json = await response.json();
+        console.log(response)
+        if (response.status >= 400) {
+            window.alert("oops mail incorrect");
+            return;
+        }
+
+        sessionStorage.setItem("token", json.token);
+        // Gestion erreur
+
+
+        navigate("/infoProfil")
     }
 
     return (
-       <div id="app" className="container-fluid row w-100 h-100 m-0 p-0">
-           <BarreNavCore/>
-           <div className="col-7 p-5">
-               <h1>Modification de l&lsquo;identifiant</h1>
+        <div id="app" className="container-fluid row w-100 h-100 m-0 p-0">
+            <BarreNavCore/>
+            <div className="col-7 p-5">
+                <h1>Modification de l&lsquo;adresse mail du profil</h1>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <TextField label="Nouvel identifiant" type="text" variant="standard" className="textfield" {...register("newID")} required/><br/>
+                    <TextField label="Nouvel adresse mail" type="email" variant="standard"
+                               className="textfield" {...register("newMail")} required/><br/>
                     <Button href="/infoProfil">Annuler</Button><Button type="submit">Valider</Button><br/>
                 </form>
-           </div>
-       </div>
-   )
+            </div>
+        </div>
+    )
 }
 
 export default ModifyID;
