d633d5236141cbba8a97b4e8e74ff5cad02d7498
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-23 17:36:55 +0100

    Fix : merge

--- front/src/Views/AccueilProfil/AccueilProfil.js
+++ front/src/Views/AccueilProfil/AccueilProfil.js
@@ -37,10 +37,8 @@ function LigneTableauProjet(data) {
             <td align={"center"} valign={"middle"}>{data.nomProjet}</td>
             <td align={"center"} valign={"middle"}>{data.etat}</td>
             <td align={"center"} valign={"middle"}>
-                {data.etat === "INPROGRESS" && <Button className="m-3" variant="secondary">Modifier</Button>}
-                <Button className="m-3" href={`/result?id=${data.id}`} variant="primary">Visionner</Button>
                 <Button className="m-3" variant="secondary" onClick={handleClick}>Modifier</Button>
-                <Button className="m-3" variant="primary">Visionner</Button>
+                <Button className="m-3" href={`/result?id=${data.id}`} variant="primary">Visionner</Button>
                 <Button className="m-3" variant="outline-primary">Créer une copie</Button>
                 <Button className="m-3" variant="outline-danger">Dissocier</Button>
             </td>
