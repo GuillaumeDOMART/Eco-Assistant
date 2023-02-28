e8a2ff8ebda0251df3f43c0e3d4b3a9d1837f97a
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-02-28 16:50:28 +0100

    Feat : calcul utilise uniquement projet

--- back/src/main/java/com/ecoassitant/back/repository/ProjetRepository.java
+++ back/src/main/java/com/ecoassitant/back/repository/ProjetRepository.java
@@ -1,6 +1,7 @@
 package com.ecoassitant.back.repository;
 
 import com.ecoassitant.back.entity.ProjetEntity;
+import com.ecoassitant.back.entity.tools.TypeP;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -29,4 +30,6 @@ public interface ProjetRepository extends JpaRepository<ProjetEntity, Integer> {
      * @return List<ProjetEntity>
      */
     List<ProjetEntity> findByProfilMail(String mail);
+
+    List<ProjetEntity> findByType(TypeP type);
 }
--- back/src/main/java/com/ecoassitant/back/service/impl/CalculServiceImpl.java
+++ back/src/main/java/com/ecoassitant/back/service/impl/CalculServiceImpl.java
@@ -6,6 +6,7 @@ import com.ecoassitant.back.dto.resultat.CalculDto;
 import com.ecoassitant.back.dto.resultat.ResultatDto;
 import com.ecoassitant.back.entity.CalculEntity;
 import com.ecoassitant.back.entity.tools.Phase;
+import com.ecoassitant.back.entity.tools.TypeP;
 import com.ecoassitant.back.repository.CalculRepository;
 import com.ecoassitant.back.repository.ProfilRepository;
 import com.ecoassitant.back.repository.ProjetRepository;
@@ -67,7 +68,7 @@ public class CalculServiceImpl implements CalculService {
         }
 
         var resultat = new ResultatsPhaseDto(mine.get());
-        var projects = projetRepository.findAll();
+        var projects = projetRepository.findByType(TypeP.PROJET);
         if (projects.isEmpty()) {
             return Optional.empty();
         }
