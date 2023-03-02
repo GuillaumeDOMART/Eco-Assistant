277c2c652981167281a353150211d829a1440505
guillaume LAGINHA <guillaume.laginha.dev@gmail.com>
2023-03-02 14:33:47 +0100

    feat: ban work

--- back/src/main/java/com/ecoassitant/back/service/impl/AdminServiceImpl.java
+++ back/src/main/java/com/ecoassitant/back/service/impl/AdminServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecoassitant.back.service.impl;
 
 import com.ecoassitant.back.dto.BanDto;
+import com.ecoassitant.back.exception.NoSuchElementInDataBaseException;
 import com.ecoassitant.back.repository.ProfilRepository;
+import com.ecoassitant.back.repository.ProjetRepository;
 import com.ecoassitant.back.service.AdminService;
 import org.springframework.stereotype.Service;
 
@@ -10,19 +12,32 @@ public class AdminServiceImpl implements AdminService {
 
     private final ProfilRepository profilRepository;
 
-    public AdminServiceImpl(ProfilRepository profilRepository) {
+    private final ProjetRepository projetRepository;
+
+    public AdminServiceImpl(ProfilRepository profilRepository, ProjetRepository projetRepository) {
         this.profilRepository = profilRepository;
+        this.projetRepository = projetRepository;
     }
 
     @Override
     public boolean ban(BanDto banDto) {
         var profil = profilRepository.findById(banDto.getIdToBan());
-        if(profil.isEmpty()){
-            return false;
+        var anonyme = profilRepository.findByMail("anonyme@demo.fr");
+        if(profil.isEmpty() || anonyme.isEmpty()){
+            throw new NoSuchElementInDataBaseException();
         }
         var profilValue = profil.get();
         profilValue.setIsAdmin(-3);
         profilRepository.save(profilValue);
+
+        var anonymeValue = anonyme.get();
+        var projects = projetRepository.findByProfil_IdProfil(banDto.getIdToBan());
+
+        for(var project : projects){
+            project.setProfil(anonymeValue);
+            projetRepository.save(project);
+        }
+
         return true;
     }
 }
