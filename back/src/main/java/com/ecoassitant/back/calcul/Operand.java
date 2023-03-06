1989ef07ce6d9a2aa35abec44042953e24f27a6c
Robin <83288270+Robin-Loup@users.noreply.github.com>
2023-03-06 10:27:53 +0100

    Fix : doc Operand

---
+++ back/src/main/java/com/ecoassitant/back/calcul/Operand.java
@@ -0,0 +1,38 @@
+package com.ecoassitant.back.calcul;
+
+/**
+ * Represent value of operation
+ */
+public class Operand implements OperationElem {
+    private final Double value;
+
+    /**
+     * constructor of Operand
+     * @param value Double of the value of the operand
+     */
+    public Operand(Double value) {
+        this.value = value;
+    }
+
+    /**
+     *  Value of the Operand
+     * @return the value
+     */
+    public Double val(){
+        return value;
+    }
+
+    @Override
+    public TypeOp type() {
+        return TypeOp.OPERANDE;
+    }
+
+    @Override
+    public int level() {
+        return 0;
+    }
+    @Override
+    public String toString(){
+        return value.toString();
+    }
+}
